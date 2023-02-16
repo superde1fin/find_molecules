c24174c15d3d6309773981afb2edf0b972890b0a
Vasilii Maksimov <kopolokrbrgr@gmail.com>
2023-02-15 19:22:22 -0500

    Correct commit

--- find_molecules.py
+++ find_molecules.py
@@ -65,8 +65,9 @@ def dist(atom1, atom2, box):
 def read_input():
     mol_num = int(input("Enter the number of molecules you wish to highlight: "))
     molecules = list()
-    cutoffs = dict()
+    cutoffs_list = list()
     for i in range(mol_num):
+        cutoffs = dict()
         molecules.append(list())
         print(f"Enter the types of atoms in the molecule {i+1}. When done enter 0")
         done = False
@@ -93,18 +94,22 @@ def read_input():
                     if molecules[i][j] not in cutoffs:
                         cutoffs[molecules[i][j]] = dict()
                     cutoffs[molecules[i][j]][molecules[i][k]] = cutoffs[molecules[i][k]][molecules[i][j]]
+        cutoffs_list.append(cutoffs)
 
-    return {"molecules": molecules, "cutoffs": cutoffs}
+
+    return {"molecules": molecules, "cutoffs": cutoffs_list}
 
 def get_molecules_ids(to_leave, atom_data):
     ids_toleave = []
     chain = to_leave["molecules"][-1]
     molecules = to_leave["molecules"][:-1]
-    for molecule in molecules:
+    for i, molecule in enumerate(molecules):
         copy_atoms = atom_data["atoms"].copy()
         for line in atom_data["atoms"]:
             if line[atom_data["type"]] in molecule:
-                ids_toleave += find_molecule(molecule, line, atom_data, to_leave["cutoffs"], copy_atoms, chain)
+                molecule_found =find_molecule(molecule, line, atom_data, to_leave["cutoffs"][i], copy_atoms, chain) 
+                ids_toleave += molecule_found["ids"]
+                copy_atoms = molecule_found["copy_atoms"].copy()
 
     return [*set(ids_toleave)]
     
@@ -116,48 +121,44 @@ def find_molecule(molecule, atom, atom_data, cutoffs, copy_atoms, chain):
     exclude = [atom[atom_data["id"]]]
     #Going backwards
     for i in range(starting_position - 1, -1, -1):
-        done = False
-        while not done:
-            closest = find_closest(atom_coords, atom_data, molecule[i], copy_atoms, exclude)
-            if closest:
-                closest_coords = (closest[atom_data["x"]], closest[atom_data["y"]], closest[atom_data["z"]])
-                distance_between = dist(atom_coords, closest_coords, atom_data["box"])
-                if distance_between > cutoffs[molecule[to_compare_index]][molecule[i]]:
-                    done = True
-                else:
-                    ids.append(closest[atom_data["id"]])
-                    exclude.append(closest[atom_data["id"]])
+        closest = find_closest(atom_coords, atom_data, molecule[i], copy_atoms, exclude)
+        if closest:
+            closest_coords = (closest[atom_data["x"]], closest[atom_data["y"]], closest[atom_data["z"]])
+            distance_between = dist(atom_coords, closest_coords, atom_data["box"])
+            if distance_between > cutoffs[molecule[to_compare_index]][molecule[i]]:
+                break
             else:
-                done = True
-        to_compare_index -= 1
+                ids.append(closest[atom_data["id"]])
+                exclude.append(closest[atom_data["id"]])
+            to_compare_index -= 1
+            atom_coords = closest_coords
+        else:
+            break
         
     #Going forward
     to_compare_index = starting_position
     for i in range(starting_position + 1, len(molecule)):
-        done = False
-        while not done:
-            closest = find_closest(atom_coords, atom_data, molecule[i], copy_atoms, exclude)
-            if closest:
-                closest_coords = (closest[atom_data["x"]], closest[atom_data["y"]], closest[atom_data["z"]])
-                distance_between = dist(atom_coords, closest_coords, atom_data["box"])
-                if distance_between > cutoffs[molecule[to_compare_index]][molecule[i]]:
-                    done = True
-                else:
-                    ids.append(closest[atom_data["id"]])
-                    exclude.append(closest[atom_data["id"]])
+        closest = find_closest(atom_coords, atom_data, molecule[i], copy_atoms, exclude)
+        if closest:
+            closest_coords = (closest[atom_data["x"]], closest[atom_data["y"]], closest[atom_data["z"]])
+            distance_between = dist(atom_coords, closest_coords, atom_data["box"])
+            if distance_between > cutoffs[molecule[to_compare_index]][molecule[i]]:
+                break
             else:
-                done = True
-        to_compare_index += 1
+                ids.append(closest[atom_data["id"]])
+                exclude.append(closest[atom_data["id"]])
+            to_compare_index += 1
+            atom_coords = closest_coords
+        else:
+            break
 
 
+    result = {"ids":[], "copy_atoms":copy_atoms}
     if len(molecule) == len(ids):
+        result["ids"] = ids
         if not chain:
-            for line in copy_atoms:
-                if line[atom_data["id"]] in ids:
-                    copy_atoms.remove(line)
-        return ids
-    else:
-        return []
+            result["copy_atoms"] = list(filter(lambda x: False if x[atom_data["id"]] in ids else False, copy_atoms))
+    return result
 
 
 def find_closest(atom, atom_data, atom_type, copy_atoms, exclude):
